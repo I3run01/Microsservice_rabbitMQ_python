f05ccd1a0152f57392b4c578457b1f941a3a6e6e
I3run01 <brunnooa.v@gmail.com>
2023-08-06 13:45:23 -0300

    testing

---
+++ producer.py
@@ -0,0 +1,19 @@
+# producer.py
+
+import pika
+
+# Connect to RabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Declare a queue (creates if not exists)
+channel.queue_declare(queue='hello_queue')
+
+# Send a message
+channel.basic_publish(exchange='',
+                      routing_key='hello_queue',
+                      body='hello')
+
+print(" [x] Sent 'hello'")
+
+connection.close()
